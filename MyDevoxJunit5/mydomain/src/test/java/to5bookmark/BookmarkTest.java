eff5c70a968fc32e06ece09e65f2dfd5e66d6d20
Rimma77 <rimma34@gmail.com>
2021-03-21 21:56:48 +0100

    2.1 BookmarkTest

--- MyDevoxJunit5/mydomain/src/test/java/to5bookmark/BookmarkTest.java
+++ MyDevoxJunit5/mydomain/src/test/java/to5bookmark/BookmarkTest.java
@@ -1,9 +1,11 @@
 package to5bookmark;
 
 import org.craftsrecords.rememberme.bookmark.Bookmark;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Assertions;
+//import org.junit.Rule;
+//import org.junit.Test;
+//import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 
@@ -13,8 +15,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BookmarkTest {
 
-    @Rule
-    public final ExpectedException exception = ExpectedException.none();
+//    @Rule
+//    public final ExpectedException exception = ExpectedException.none();
 
     @Test
     public void should_create_the_bookmark() {
@@ -22,28 +24,39 @@ public class BookmarkTest {
         String name = "Some name";
         Set<String> tags = singleton("tag");
         Bookmark createdBookmark = Bookmark.create(url, name, tags);
+        
+        Assertions.assertAll(
+        		() ->         assertThat(createdBookmark.getUrl()).isEqualTo(url) ,
+        		 () ->       assertThat(createdBookmark.getName()).isEqualTo(name),
+        		 () ->       assertThat(createdBookmark.getTags()).isEqualTo(tags) 
+        		 );
 
-        assertThat(createdBookmark.getUrl()).isEqualTo(url);
-        assertThat(createdBookmark.getName()).isEqualTo(name);
-        assertThat(createdBookmark.getTags()).isEqualTo(tags);
+//        assertThat(createdBookmark.getUrl()).isEqualTo(url);
+//        assertThat(createdBookmark.getName()).isEqualTo(name);
+//        assertThat(createdBookmark.getTags()).isEqualTo(tags);
     }
 
     @Test
     public void should_not_accept_an_invalid_url() {
-        exception.expect(IllegalArgumentException.class);
-        Bookmark.create("invalid_url", "name", emptySet());
+    	Assertions.assertThrows(IllegalArgumentException.class,
+    		() ->	Bookmark.create("invalid_url", "name", emptySet()));
+//    	
+//        exception.expect(IllegalArgumentException.class);
+//        Bookmark.create("invalid_url", "name", emptySet());
     }
 
     @Test
     public void should_not_accept_an_empty_name() {
-        exception.expect(IllegalArgumentException.class);
-        Bookmark.create("http://www.test.com", "", emptySet());
+    	Assertions.assertThrows(IllegalArgumentException.class, 
+    		() ->	Bookmark.create("http://www.test.com", "", emptySet()));
+    	
     }
 
     @Test
     public void should_not_accept_an_blank_name() {
-        exception.expect(IllegalArgumentException.class);
-        Bookmark.create("http://www.test.com", "  ", emptySet());
+    	Assertions.assertThrows(IllegalArgumentException.class,  
+    	() ->		 Bookmark.create("http://www.test.com", "  ", emptySet()));
+      
     }
 
 }

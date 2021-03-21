b9fd11b77a1d5357ba80254a2d5b9e3afcc0a77e
Rimma77 <rimma34@gmail.com>
2021-03-21 20:03:55 +0100

    2.1 BookmarksFinderTest ok

--- MyDevoxJunit5/mydomain/src/test/java/to5bookmark/BookmarksFinderTest.java
+++ MyDevoxJunit5/mydomain/src/test/java/to5bookmark/BookmarksFinderTest.java
@@ -1,54 +1,44 @@
 package to5bookmark;
 
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
 import org.craftsrecords.rememberme.api.FindBookmarks;
 import org.craftsrecords.rememberme.bookmark.Bookmark;
 import org.craftsrecords.rememberme.bookmark.Bookmarks;
 import org.craftsrecords.rememberme.bookmark.BookmarksFinder;
 import org.craftsrecords.rememberme.stubs.InMemoryBookmarks;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
-import java.util.Collection;
-
-import static java.util.Arrays.asList;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.runners.Parameterized.Parameter;
-
-@RunWith(Parameterized.class)
 public class BookmarksFinderTest {
-
+                                                                        
     private static Bookmarks bookmarks;
 
-    @Parameter
-    public String tag;
-
-    @Parameter(1)
-    public Bookmark[] expected;
-
-    @Test
-    public void should_find_bookmarks_by_tag() {
+    @ParameterizedTest
+    @MethodSource("testCases2")
+    public void should_find_bookmarks_by_tag(String tag, Bookmark[] expected) {
         FindBookmarks findBookmarks = new BookmarksFinder(bookmarks);
         Collection<Bookmark> bookmarks = findBookmarks.by(tag);
 
         assertThat(bookmarks).containsExactlyInAnyOrder(expected);
     }
 
-    @Parameters
-    public static Collection<Object[]> testCases() {
+    public static Stream<Arguments> testCases2() {
         bookmarks = new InMemoryBookmarks();
 
         Bookmark junit = saveBookmark("https://junit.org", "JUnit", "tests");
         Bookmark cucumber = saveBookmark("https://cucumber.io", "Cucumber", "tests", "bdd");
         Bookmark bdd = saveBookmark("https://en.wikipedia.org/wiki/BDD", "BDD", "bdd", "methodo");
-
-        return Arrays.asList(new Object[][]{
-                {"tests", new Bookmark[]{junit, cucumber}},
-                {"bdd", new Bookmark[]{cucumber, bdd}},
-                {"methodo", new Bookmark[]{bdd}}
-        });
+ 
+        return   Stream.of(Arguments.arguments("tests", new Bookmark[]{junit, cucumber} ),
+        		Arguments.arguments( "bdd", new Bookmark[]{cucumber, bdd}),
+        		Arguments.arguments("methodo", new Bookmark[]{bdd} )
+        		);
     }
 
     private static Bookmark saveBookmark(String url, String name, String... tags) {
